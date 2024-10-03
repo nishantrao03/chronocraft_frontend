5336339318aeec16ca953ae49f6177572c5c76ef
Nishant Rao <nishantrao2003@gmail.com>
2024-10-03 20:38:51 +0530

    Initial commit 2

--- src/firebase.js
+++ src/firebase.js
@@ -5,18 +5,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBA9bz1kzHBI0BWbLV-wUca3y5BzM2vEns",
-//   authDomain: "chronocraft-2f3e1.firebaseapp.com",
-//   projectId: "chronocraft-2f3e1",
-//   storageBucket: "chronocraft-2f3e1.appspot.com",
-//   messagingSenderId: "365471803777",
-//   appId: "1:365471803777:web:89f5a0cd53fe4832a1e080",
-//   measurementId: "G-709VKT9F95"
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
