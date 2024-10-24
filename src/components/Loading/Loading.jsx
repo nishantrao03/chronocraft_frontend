d02dd7accdc75ee622477042295e4270c2182112
Nishant Rao <nishantrao2003@gmail.com>
2024-10-24 12:08:11 +0530

    Modified loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,26 @@
 // Loading.js
 import React from "react";
-import styles from "./Loading.module.css"; // Import the CSS module
+import styles from "./Loading.module.css";
+import { useEffect } from "react";
+import axios from "axios"; // Import the CSS module
 
 const Loading = () => {
+
+  useEffect(() => {
+    // Function to make an API call to the backend
+    const wakeBackend = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/healthcheck`);
+        console.log('Backend is awake:', response.status);
+      } catch (error) {
+        console.error('Error waking up the backend:', error);
+      }
+    };
+
+    // Call the function to wake up the backend
+    wakeBackend();
+  }, []); // Empty dependency array ensures this runs only once when the component is mounted
+
   return (
     <div className={styles.loadingContainer}>
       {/* <div className={styles.logoContainer}>
